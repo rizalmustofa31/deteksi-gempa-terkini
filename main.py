d19fd3a45ad91ce6a1f8c03319518e3476c8e49d
Rizal Mustofa <mustofarizal1994@gmail.com>
2022-02-15 21:29:25 +0700

    deteksi gempa bmkg

--- main.py
+++ main.py
@@ -1,41 +1,9 @@
 """
 Aplikasi deteksi gempa terkini
 """
-
-
-def ekstrasi_data():
-    """
-    Tanggal: 04 Februari 2022
-    Waktu: 17:10:45 WIB
-    Magnitudo: 5.5
-    Kedalaman: 10 km
-    Lokasi: LS=7.48 LS - BT=105.92 BT
-    Pusat gempa: Pusat gempa berada di laut 71 Km Barat Daya Bayah
-    Dirasakan: Dirasakan (Skala MMI): IV Pelabuhan Ratu, III Malingping, III Bayah, III Cihara, III Panggarangan
-    :return:
-    """
-    hasil = dict()
-    hasil['tanggal'] = '04 Februari 2022'
-    hasil['waktu'] = '17:10:45 WIB'
-    hasil['magnitudo'] = 5.5
-    hasil['lokasi'] = {'ls': 7.48, 'bt': 105.92 }
-    hasil['pusat'] = 'Pusat gempa berada di laut 71 Km Barat Daya Bayah'
-    hasil['dirasakan'] = 'Dirasakan (Skala MMI): IV Pelabuhan Ratu, III Malingping, III Bayah, III Cihara, III Panggaran'
-
-    return hasil
-
-
-def tampilkan_data(result):
-    print('Gempa terakhir berdasarkan BMKG')
-    print(f"Tanggal {result['tanggal']}")
-    print(f"Waktu {result['waktu']}")
-    print(f"Magnitudo {result['magnitudo']}")
-    print(f"Lokasi: LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
-    print(f"Pusat {result['pusat']}")
-    print(f"Dirasakan {result['dirasakan']}")
-
+import gempaterkini
 
 if __name__ == '__main__':
     print('Aplikasi utama')
-    result = ekstrasi_data()
-    tampilkan_data(result)
+    result = gempaterkini.ekstrasi_data()
+    gempaterkini.tampilkan_data(result)
